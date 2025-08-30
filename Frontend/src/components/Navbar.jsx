cd586447ccaacc9a068415652d100a717f35adae
Himanshukabsuri <hkabsuri@gmail.com>
2025-08-30 18:29:16 +0530

    add feedback

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -13,16 +13,19 @@ const Navbar = () => {
   return (
     <>
       {/* Navbar */}
-      <div className="fixed z-50 w-full flex items-center justify-between h-30 px-0">
-        <img src={logo}  alt="logo" className="h-38 w-38 ml-0 cursor-pointer" />
+      <div className='fixed h-26 z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 '>
 
-        <button
-          onClick={() => setOpen(true)}
-          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-blue-500 text-white px-10 py-2.5"
-        >
-          Get Started
-        </button>
-      </div>
+            <img src={logo} alt="logo" className=' w-32 sm:w-44 cursor-pointer'  />
+
+            
+              
+                    <button  className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get Started </button>
+                
+            
+
+
+
+        </div>
 
       {/* Popup (Modal) */}
       {open && (
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Tools from '../components/Tools'
+import Feedback from '../components/Feedback'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar/>
         <Hero/>
         <Tools/>
+        <Feedback/>
     </div>
   )
 }
