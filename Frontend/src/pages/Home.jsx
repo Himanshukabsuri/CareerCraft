f8260f87369dfd017e502c965a32850f70fa03c0
Himanshukabsuri <hkabsuri@gmail.com>
2025-08-30 12:57:00 +0530

    Adding card tools

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Tools from '../components/Tools'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Hero/>
+        <Tools/>
     </div>
   )
 }
